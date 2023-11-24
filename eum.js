c74a17933042fcb55d45747947d7d280bfe5cc1b
Thisoe <thisoecode@gmail.com>
2023-11-24 23:04:31 +0900

    [0.0.1.1] made the Regex of notes

---
+++ eum.js
@@ -0,0 +1,18 @@
+class Eum {
+
+//CONFIG
+    #A4 = 440
+    #itv = Math.pow(2, 1/12)
+    #regexNote = /^([A-Ga-g])(#|b)?(\d+)$/
+
+//CONSTRUCT
+    constructor(){
+        this.$=[]
+    }
+
+//FUNCS
+    compile(){
+
+    }
+
+}
