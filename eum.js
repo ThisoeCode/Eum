558375c80ca40412d99e628086eec6f43dfdbc16
Thisoe <thisoecode@gmail.com>
2023-11-26 15:24:06 +0900

    [0.0.1.2] build save

--- eum.js
+++ eum.js
@@ -1,18 +1,69 @@
 class Eum {
 
-//CONFIG
+//CONSTS
     #A4 = 440
     #itv = Math.pow(2, 1/12)
-    #regexNote = /^([A-Ga-g])(#|b)?(\d+)$/
+
+    // Config Definition Regexs
+    #isNote =/^([A-Ga-g])(#|b)?(\d+)$/
+    #isSigna=/^(2|3|4|5|6|7|8|9|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9]|6[0-9]|7[0-9]|8[0-9]|9[0-9]|1[0-5][0-9])(\/)(1|2|4|8|16|32|64)$/
+
+    // Cipher Regexs
+    #isDef  =/^\[$/
+    #isSound=/^0-6$/
+    #isBar  =/^\/$/
 
 //CONSTRUCT
-    constructor(){
+    constructor(EUM){
+        /**
+         * @name Eson
+         * @description Compiled stuff.
+         */
         this.$=[]
+
+        // compile(E) config
+        this.note  = 'c'
+        this.signa = '4/4'
+        this.tempo = 120
+        this.currNote  = this.note
+        this.currSigna = this.signa
+        this.currTempo = this.tempo
+        this.currCursor= 0
+
+        /** Length of inputted string */
+        this.l=0
+
+        this.newSound = null
+        this.newLength= 0
+
+        compile(EUM)
     }
 
 //FUNCS
-    compile(){
+    /**
+     * Compile from Eum to Eson
+     * @param {string} E - Eum content
+     * @returns {undefined | false | null | string}
+     */
+    compile(E){if(typeof E !== 'string'){
+        return false;
+    }else{
+        this.l = E.length
+        this.$.push({
+            pitch: null,
+            time:  1000,
+        })
+        const recognize = i=>{
+            switch (E[i]) {
+                case value:
+                    
+                    break;
+            
+                default:
+                    break;
+            }
+        }
 
-    }
+    }}
 
 }
