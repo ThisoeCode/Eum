43b73801d736a913821b6889a2fa073769376a20
Thisoe <thisoecode@gmail.com>
2023-11-27 22:24:42 +0900

    [0.0.1.6] Throw errors

--- eum.js
+++ eum.js
@@ -8,8 +8,9 @@ class Eum {
   #itv = Math.pow(2, 1 / 12)
 
   // Config Definition Regexs
-  #isNote = /^([A-Ga-g])(#|b)?(\d+)$/
-  #isSigna = /^(2|3|4|5|6|7|8|9|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9]|6[0-9]|7[0-9]|8[0-9]|9[0-9]|1[0-5][0-9])(\/)(1|2|4|8|16|32|64)$/
+  #isNote = /^( +)?1( +)?=( +)?([A-Ga-g])(#|b)?(m)?([0-9]|1[0-2])?( +)?$/
+  #isTempo = /^( +)?(V|v|T|t)( +)?=( +)?(\d+)( +)?$/
+  #isSigna = /^( +)?([2-9]|[1-9][0-9]|1[0-9][0-9])( +)?(\/)( +)?(1|2|4|8|16|32|64)( +)?$/
 
   // Cipher Regexs
   #isDef = /^\[$/
@@ -26,12 +27,12 @@ class Eum {
     this.$ = []
 
     // compile(E) config
-    this.defaultNote = 'c'
-    this.defaultSigna = '4/4'
+    this.defaultNote = 'C4'
     this.defaultTempo = 120
+    this.defaultSigna = '4/4'
     this.note = this.defaultNote
-    this.signa = this.defaultSigna
     this.tempo = this.defaultTempo
+    this.signa = this.defaultSigna
     this.cursor = 0
 
     /** Length of inputted string of Eum */
@@ -43,30 +44,67 @@ class Eum {
     compile(EUM)
   }
 
-  //FUNCS
+  //METHODS
+
+  /**
+   * Calculate note
+   * @param {string} n Note name
+   * @returns {number} Frequency (Hz)
+   * @throws Not a note
+   */
+  calcNote(n, msg = '') {
+    if (!this.#isNote.test(n)) {
+      const err = `Eum error:\n"${n}" is not a note`
+      if (msg !== '') { err += `\n${msg}` }
+      throw new Error(err)
+    } else {
+      return 0// ???????????????
+    }
+  }
+
+  /**
+   * Calculate tempo
+   * @param {number | string} t Tempo
+   * @returns {number} Delay time (ms)
+   * @throws Not a tempo
+   */
+  calcTempo(t, msg = ''){
+    if(t<0){
+      const err = `Eum error:\n"${t}" is not a tempo`
+      if (msg !== '') { err += `\n${msg}` }
+      throw new Error(err)
+    }else{
+      return 0// ???????????????
+    }
+  }
+
   /**
    * Compile from Eum to Eson
    * @param {string} E - Eum content
-   * @returns {undefined | Error}
+   * @returns {undefined}
+   * @throws Compile errors
    */
   compile(E) {
+    // error handler
     const positionOf = i => {
       const lines = E.split('\n')
       for (let m = 0; m < lines.length; m++) {
         const line = lines[m]
         const charIndex = line.indexOf(E[i])
         if (charIndex !== -1) {
-          return `${i + 1}:${charIndex + 1}`;
+          return `${i + 1}:${charIndex + 1}`
         }
       }
     }
     const syntaxError = (i, msg = 'unknown error') => {
-      return new Error(`[Eum] BAD SYNTAX: ${msg} - AT ${positionOf(i)}`);
+      throw new Error(`Eum compiler error:\nBAD SYNTAX: ${msg}\n    AT [EumString]:${positionOf(i)}`)
     }
+
+    // main
     if (typeof E !== 'string') {
-      return new Error('Eum compiler expects a string type parameter.');
+      throw new Error('[Eum] Eum compiler expects a string type parameter')
     } else {
-      // init
+      // init constructor
       this.eumLength = E.length
 
       // funcs
@@ -74,14 +112,22 @@ class Eum {
         const x = E[i]
         if (x === '[') {
           const close = E.indexOf(']', i)
-          if (close === -1) {
-            return syntaxError(i, '"]" expected')
+          this.cursor = close
+          const ctt = x.slice(i + 1, close)
+          switch (true) {
+            case this.#isNote.test(x):
+              this.note = x.toUpperCase
+              break
+            case this.#isSigna.test(x):
+              this.signa = x
+              break
+            case this.#isTempo.test(x):
+              this.tempo
           }
+          if (close === -1) { syntaxError(i, '"]" expected') }
         } else if (1) {
 
-        } else {
-          return syntaxError(i, `unexpected token "${x}"`)
-        }
+        } else { syntaxError(i, `Unexpected token "${x}"`) }
       }
 
       // run compile
@@ -98,9 +144,10 @@ class Eum {
 
   /**
    * Use Eson to play audio
-   * @param {string} [instrument] - Instruments name. If is not in ['sine', 'square', 'sawtooth', or 'triangle'] , the second param is needed.
-   * @param {string} [src] - Audio file path for custom instrument.
-   * @returns {undefined | false}
+   * @param {string} [instrument] Instruments name. If is not in ['sine', 'square', 'sawtooth', or 'triangle'] , the second param is needed.
+   * @param {string} [src] Audio file path for custom instrument.
+   * @returns {undefined}
+   * @throws Compile errors
    */
   play(instrument = 'sine', src = '') {
     for (const e of this.$) {
@@ -115,7 +162,7 @@ class Eum {
         case 'square':
         case 'sawtooth':
         case 'triangle':
-          
+
         default:
           // get src sound file
           const request = new XMLHttpRequest()
